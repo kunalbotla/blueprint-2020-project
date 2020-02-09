394c2a431b65bec7f11baa4e5190735ab4583e09
thecoder12345678 <60804131+thecoder12345678@users.noreply.github.com>
2020-02-09 08:58:14 -0600

    Created files

---
+++ input.py
@@ -0,0 +1,8 @@
+# takes the input/data of the user 
+c = input("What did you consume?" + " Drive your car (1 mi)?" + " Eat 100 grams of food?")
+if c == "Drive your car (1 mi)" :
+    print("car")
+elif c == "Eat 100 grams of food" : 
+    print("food")
+else: 
+    print("Write here!")
